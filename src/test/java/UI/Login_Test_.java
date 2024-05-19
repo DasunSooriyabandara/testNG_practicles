eae9aaddbc615cf8381923ff306032b85130529b
Dasun Sooriyabandara <115138972+DasunSooriyabandara@users.noreply.github.com>
2024-05-19 17:37:45 +0530

    First test

---
+++ src/test/java/UI/Login_Test_.java
@@ -0,0 +1,19 @@
+package UI;
+
+import org.testng.annotations.Test;
+
+public class Login_Test_ {
+
+	
+	@Test(priority=1,description= ": Login Test")
+	public void BLogin() {
+		System.out.println("successfully Login");
+
+	}
+	@Test(priority=2,description= ":LogOut Test")
+	public void ALogOut() {
+		System.out.println("successfully LogOut");
+
+	}
+	
+}
